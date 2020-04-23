9ad0104adec783385cb2527a5156f34b8f2b4d59
FratStar <lemueljwilson@gmail.com>
2020-04-23 15:36:55 -0400

    Done

--- Backend/app.py
+++ Backend/app.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from fastapi import FastAPI, Response, Depends, Header
 from fastapi.security import HTTPBasicCredentials, HTTPBearer
 from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import JSONResponse, Response, UJSONResponse
 from pydantic import BaseModel
 from typing import Optional, Dict, List
 
@@ -11,14 +12,17 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:4200",
+    "http://localhost:4200/",
     "https://localhost",
     "https://localhost:4200",
+    "https://localhost:4200/",
+    
 ]
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -55,6 +59,8 @@ def createToken(input):
 
 
 def decodeToken(authorization : HTTPBasicCredentials = Depends(token_check)):
+    if authorization is None:
+        return None
     for i in authorization:
         token = i[1]
 
@@ -74,12 +80,16 @@ async def set_name(username: User):
     token = createToken(token_data)
 
     if any(token_data["username"].lower().startswith(x) for x in json_allowed):
-        settings.name.append(token_data["username"])
+        if(token_data["username"] not in settings.name):
+            settings.name.append(token_data["username"])
+
         content = { "token": { "access_token": token, "type": "Bearer", "exp": token_data["exp"]} }
         return content
 
     elif any(token_data["username"].lower().startswith(x) for x in xml_allowed):
-        settings.name.append(token_data["username"])
+        if(token_data["username"] not in settings.name):
+            settings.name.append(token_data["username"])
+
         data = """<?xml version="1.0"?>
         <token><access_token>{}</access_token>
         <type>Bearer</type>
@@ -90,7 +100,7 @@ async def set_name(username: User):
         return content
     
     else:
-        content = {"message": "No Data Provided"}
+        content = {"Result": "No Data Provided"}
         return content
 
     return
@@ -98,34 +108,34 @@ async def set_name(username: User):
 
 
 @app.get("/ready")
-async def get_ready(token: str = Header(None)):
-    token_data = jwt.decode(token, 'secret', algorithms='HS256')
-    print(settings.name)
-    print(token_data["username"])
-
-    if token_data["username"] not in settings.name:
-        info = {"message": "No Name Input"}
+async def get_ready(Authorization: HTTPBasicCredentials = Depends(decodeToken)):
+    print(Authorization)
+    if Authorization is None:
+        print("done")
+        info = {"Result": "No Name Input"}
         return info
-        
-    elif (any(Authorization.username.lower().startswith(x) for x in json_allowed) and Authorization.username in settings.name):
-        info = {"message": "Calulator is ready",
-                "user": settings.name}
+    
+    if (Authorization.username not in settings.name):
+        settings.name.append(Authorization.username)
+        print(settings.name)
+    
+
+    if (any(Authorization.username.lower().startswith(x) for x in json_allowed) and Authorization.username in settings.name):
+        print("done")
+        info = {"Result": "Calulator is ready"}
         return info
 
     elif (any(Authorization.username.lower().startswith(x) for x in xml_allowed) and Authorization.username in settings.name):
+        print("xdone")
         info = """<?xml version="1.0"?>
-        <message>Calulator is ready</message>
+        <Result>Calulator is ready</Result>
         """
         return Response(content=info, media_type="text/xml", status_code=201)
-
-    return
 
 
 
 @app.post("/add")
 async def perform_addition(ops: Operands, Authorization: HTTPBasicCredentials = Depends(decodeToken) ):
-
-    answer = None
 
     if ops.y is None:
         answer = {"Result": "Can't perform operation"}
@@ -143,7 +153,7 @@ async def perform_addition(ops: Operands, Authorization: HTTPBasicCredentials = 
         return Response(content=answer, media_type="text/xml", status_code=201)
 
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
 
 
@@ -167,10 +177,8 @@ async def perform_subtraction(ops: Operands, Authorization: HTTPBasicCredentials
         return Response(content=answer, media_type="text/xml", status_code=201)
 
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
-
-    return
 
 
 @app.post("/divide")
@@ -192,10 +200,8 @@ async def perform_division(ops: Operands, Authorization: HTTPBasicCredentials = 
         return Response(content=answer, media_type="text/xml", status_code=201)
 
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
-    
-    return
 
 
 @app.post("/multi")
@@ -216,10 +222,8 @@ async def perform_multi(ops: Operands, Authorization: HTTPBasicCredentials = Dep
         return Response(content=answer, media_type="text/xml", status_code=201)
 
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
-
-    return
 
 
 
@@ -242,17 +246,14 @@ async def perform_sqrt(ops: Operands, Authorization: HTTPBasicCredentials = Depe
         return Response(content=answer, media_type="text/xml", status_code=201)
 
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
-
-    return
-
 
 
 
 @app.post("/natlog")
 async def perform_natlog(ops: Operands, Authorization: HTTPBasicCredentials = Depends(decodeToken) ):
-
+    print(Authorization)
     if ops.x is None:
         answer = {"Result": "Can't perform operation"}
         return answer
@@ -269,10 +270,9 @@ async def perform_natlog(ops: Operands, Authorization: HTTPBasicCredentials = De
         return Response(content=answer, media_type="text/xml", status_code=201)
 
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
 
-    return
 
 
 @app.post("/exponential")
@@ -294,10 +294,8 @@ async def perform_exponential(ops: Operands, Authorization: HTTPBasicCredentials
         return Response(content=answer, media_type="text/xml", status_code=201)
     
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
-
-    return
 
 @app.post("/factorial")
 async def perform_factorial(ops: Operands, Authorization: HTTPBasicCredentials = Depends(decodeToken) ):
@@ -319,10 +317,9 @@ async def perform_factorial(ops: Operands, Authorization: HTTPBasicCredentials =
         return Response(content=answer, media_type="text/xml", status_code=201)
 
     else:
-        answer = {"message": "Unable to perfrom"}
+        answer = {"Result": 00000}
         return answer
 
-    return
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host='0.0.0.0', port=5000, reload=True)
